af50b2c0108a2f19f9c1c1852bece7a27c42cea9
EvgenPrushk <ogmprushak@mail.ru>
2021-04-02 12:48:38 +0300

    feat: add lesson 4

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,17 +1,51 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM, { render } from "react-dom";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-function tick() {
-  const element = (
+//компанент ввиде функции
+// входные параметры props нельзя изменять
+// использовать лучше функциональные компоненты
+function Welcom(props) {
+  return <p> Hello {props.user.name} {props.user.surname} </p>;
+}
+
+// class Welcom extends React.Component {
+//   render() {
+//     return <p> Hello {this.props.user.name} {this.props.user.surname}! </p>;
+//   }
+// }
+
+function App(props) {
+  const users = [
+    {name: "Maks", surname: "Natti"},
+    {name: "Sergei", surname: "Tatti"},
+    {name: "Tamara", surname: "Matti"},
+    {name: "Olga", surname: "Ostrovskaia"},
+  ]
+  return (
     <div>
-      <h1> Hello world</h1>
-      <h2>It is {new Date().toLocaleTimeString()} </h2>
+      <Welcom user={users[0]} />
+      <Welcom user={users[1]} />
+      <Welcom user={users[2]} />
+      <Welcom user={users[3]} />
     </div>
   );
-  ReactDOM.render(element, document.getElementById('root'));
 }
-setInterval(tick, 1000);
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+//  const elemet = <Welcom userName="Alex" />;
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1> Hello world</h1>
+//       <h2>It is {new Date().toLocaleTimeString()} </h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+// setInterval(tick, 1000);
 
 // const ul1 = (
 //   <ul>
