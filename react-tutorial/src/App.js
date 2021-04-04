7094cedc3da68579ae82acc112b8a246fbc108be
EvgenPrushk <ogmprushak@mail.ru>
2021-04-04 11:48:28 +0300

    feat: add lesson 8

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,68 +1,26 @@
 import React from "react";
+// import { useState } from "react";
+import Button from "./Button";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: 0,
-    };
-    // используем привязку контекста
-   this.handlerClick = this.handlerClick.bind(this);
-  }
-  handlerClick () {
-    this.setState({ counter: this.state.counter + 1 });
+function App(props) {
+  const handlerThirdClick = () => {
+    console.log("3-й клик выполнен");
   };
-  render() {
-    
-    return (
-      <button onClick={this.handlerClick}>
-        На меня нажила {this.state.counter} раз
-      </button>
-    );
-  }
+  const handlerClick = () => {
+    console.log("handlerClick fired");
+  };
+  const handlerMouseEnter = () => {
+    console.log("onMouseEnter called");
+  };
+  return (
+    <Button
+      onThirdClick={handlerThirdClick}
+      onClick={handlerClick}
+      onMouseEnter={handlerMouseEnter}
+    >
+      Новый текст для кнопки
+    </Button>
+  );
 }
-
-// class App extends React.Component {
-//   // осуществляется монтирование
-//   constructor(props) {
-//     super(props);
-
-//     this.intervalFlag = null;
-
-//     this.state = {
-//       title: "Текущее время",
-//       date: new Date(),
-//       counter: 0,
-//     };
-//   }
-//   // происходит состояние монтирования
-//   componentDidMount() {
-//     this.intervalFlag = setInterval(() => {
-//       this.setState((state, props) => ({
-//         date: new Date(),
-//         counter: state.counter + 1,
-//       }));
-//     }, 1000);
-//   }
-//   // происходит состояние обнавления
-//   componentDidUpdate() {
-//     console.log("componentDidUpdate called");
-//   }
-//   // происходит состояние размонтирования
-//   componentWillUnmount() {
-//     clearInterval(this.intervalFlag);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <p>{this.state.title}</p>
-//         <p>{this.state.counter}</p>
-//         <p>{this.state.date.toLocaleTimeString()}</p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
