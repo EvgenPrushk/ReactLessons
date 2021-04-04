1d6ae670da5d73acc71e2461648fec33ca9cdd55
EvgenPrushk <ogmprushak@mail.ru>
2021-04-04 09:57:51 +0300

    feat: add lesson8

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,45 +1,68 @@
 import React from "react";
 
 class App extends React.Component {
-  // осуществляется монтирование
   constructor(props) {
     super(props);
 
-    this.intervalFlag = null;
-
     this.state = {
-      title: "Текущее время",
-      date: new Date(),
       counter: 0,
     };
+    // используем привязку контекста
+   this.handlerClick = this.handlerClick.bind(this);
   }
-  // происходит состояние монтирования
-  componentDidMount() {
-    this.intervalFlag = setInterval(() => {
-      this.setState((state, props) => ({
-        date: new Date(),
-        counter: state.counter + 1,
-      }));
-    }, 1000);
-  }
-  // происходит состояние обнавления
-  componentDidUpdate() {
-    console.log("componentDidUpdate called");
-  }
-  // происходит состояние размонтирования
-  componentWillUnmount() {
-    clearInterval(this.intervalFlag);
-  }
-
+  handlerClick () {
+    this.setState({ counter: this.state.counter + 1 });
+  };
   render() {
+    
     return (
-      <div>
-        <p>{this.state.title}</p>
-        <p>{this.state.counter}</p>
-        <p>{this.state.date.toLocaleTimeString()}</p>
-      </div>
+      <button onClick={this.handlerClick}>
+        На меня нажила {this.state.counter} раз
+      </button>
     );
   }
 }
+
+// class App extends React.Component {
+//   // осуществляется монтирование
+//   constructor(props) {
+//     super(props);
+
+//     this.intervalFlag = null;
+
+//     this.state = {
+//       title: "Текущее время",
+//       date: new Date(),
+//       counter: 0,
+//     };
+//   }
+//   // происходит состояние монтирования
+//   componentDidMount() {
+//     this.intervalFlag = setInterval(() => {
+//       this.setState((state, props) => ({
+//         date: new Date(),
+//         counter: state.counter + 1,
+//       }));
+//     }, 1000);
+//   }
+//   // происходит состояние обнавления
+//   componentDidUpdate() {
+//     console.log("componentDidUpdate called");
+//   }
+//   // происходит состояние размонтирования
+//   componentWillUnmount() {
+//     clearInterval(this.intervalFlag);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.title}</p>
+//         <p>{this.state.counter}</p>
+//         <p>{this.state.date.toLocaleTimeString()}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
