66fa6ab2ed71aff75434789aa3b20073f5d0ae6c
EvgenPrushk <ogmprushak@mail.ru>
2021-04-05 17:23:30 +0300

    feat: add lesson 11

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,35 +1,50 @@
 import React from "react";
 // import { useState } from "react";
-// import { useState } from "react";
-// import Content from "./Content";
-// import RecursiveButton from "./RecursiveButton";
+import { useState } from "react";
+import Content from "./Content";
+import RecursiveButton from "./RecursiveButton";
 
 function App(props) {
-  const users = [
-    { id: 1, name: "ALex", surename: "Petrov" },
-    { id: 2, name: "Maks", surename: "Grabov" },
-    { id: 3, name: "Mihail", surename: "Nokov" },
-    { id: 4, name: "Sergei", surename: "Matov" },
-    { id: 5, name: "Maks", surename: "Bury" },
-  ];
+  const [checked, setChecked] = useState(false);
 
-  const handlerMouseEnter = (user) => {
-    console.log(`${user.name} ${user.surename}`);
-  };
-  // кей идет у того компонента, который идет в качестве элемента массива
-  let lis = users.map((user, index) => {
-    let style = {};
-    const even = index % 2 === 0;
-    if (even) {
-      style = { color: "white", background: "black" };
-    }
-    return (
-      <li key={user.id} style={style} onMouseEnter={() => handlerMouseEnter(user)}>
-        {user.name} {user.surename}
-      </li>
-    );
-  });
-  return <ul>{lis}</ul>;
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          />
+          Какое-то значениe
+      </label>
+    </div>
+  );
 }
+// 09-31
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
 
+//     this.state = {
+//       value: "AText",
+//     };
+//   }
+//   hadlerChange(event) {
+//     this.setState({
+//       value: event.target.event,
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {this.state.value}
+//         <br />
+//         <input
+//           value={this.state.value}
+//           onChange={(e) => this.hadlerChange(e)}
+//         />
+//       </div>
+//     );
+//   }
+// }
 export default App;
