caba55e3b7c1dd319e61c16b018cf0ca6b81caa4
EvgenPrushk <ogmprushak@mail.ru>
2021-04-05 10:56:28 +0300

    feat: add lesson10

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,27 +1,35 @@
 import React from "react";
-import { useState } from "react";
 // import { useState } from "react";
-import Content from "./Content";
-import RecursiveButton from "./RecursiveButton";
+// import { useState } from "react";
+// import Content from "./Content";
+// import RecursiveButton from "./RecursiveButton";
 
 function App(props) {
-  const [showMode, setShowMode] = useState(true);
+  const users = [
+    { id: 1, name: "ALex", surename: "Petrov" },
+    { id: 2, name: "Maks", surename: "Grabov" },
+    { id: 3, name: "Mihail", surename: "Nokov" },
+    { id: 4, name: "Sergei", surename: "Matov" },
+    { id: 5, name: "Maks", surename: "Bury" },
+  ];
 
-  const handlerClick = () => {
-    setShowMode(!showMode);
+  const handlerMouseEnter = (user) => {
+    console.log(`${user.name} ${user.surename}`);
   };
-  let content = null;
-  if (showMode) {
-    content = <Content />;
-  } else {
-  }
-  return (
-    <div>
-      <RecursiveButton />
-      <button onClick={handlerClick}>Show toggle</button>
-      {content}
-    </div>
-  );
+  // кей идет у того компонента, который идет в качестве элемента массива
+  let lis = users.map((user, index) => {
+    let style = {};
+    const even = index % 2 === 0;
+    if (even) {
+      style = { color: "white", background: "black" };
+    }
+    return (
+      <li key={user.id} style={style} onMouseEnter={() => handlerMouseEnter(user)}>
+        {user.name} {user.surename}
+      </li>
+    );
+  });
+  return <ul>{lis}</ul>;
 }
 
 export default App;
