46c601f7ad5b96a695a81b30b627fc7b3e703770
EvgenPrushk <ogmprushak@mail.ru>
2021-04-29 14:28:36 +0300

    feat: add lesson13

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,49 +1,115 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 // import Content from "./Content";
-// import RecursiveButton from "./RecursiveButton";
-import CityesList from "./CitiesList";
-import City from "./City";
+// import RecfunursiveButton from "./RecursiveButton";
+// import CityesList from "./CitiesList";
+// import City from "./City";
 
 function App() {
-  const [cities, setCities] = useState([
-    { name: "Moscow", description: "Столица России" },
-    { name: "Zelenograd", description: "Научный спутник москвы" },
-  ]);
+  const [counter, setCounter] = useState(0);
 
-  // the index of the city we want to display
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlerChangeCity = (n, description) => {
-    setCities(
-      cities.map((city, index) => {
-        if (index === n) {
-          return {
-            ...city,
-            description,
-          };
-        }
-        return city;
-      })
-    );
+  const handlerClick = () => {
+    // doing synx 0 + 1
+    setCounter((x, props) => x + 1);
+    //  and doing asynx 0 + 1
+    setCounter((x, props) => x + 1);
   };
 
-  const handlerSelectCity = (n) => setCurrentIndex(n)
-
-  
-
-  return (
-    <div>
-      <City
-        cities={cities}
-        onChangeCity={handlerChangeCity}
-        currentIndex={currentIndex}
-      />
-      <CityesList cities={cities} onSelectCity ={handlerSelectCity}/>
-    </div>
-  );
+  return <button onClick={handlerClick}> На меня нажали {counter} раз</button>;
 }
+
+// function App() {
+//   const [state, setState] = useState({
+//     counter: 0,
+//     user: {
+//       name: "Maks",
+//       surename: "Petrov",
+//     },
+//   });
+
+//   const handlerClick = (e) => {
+//     setState({
+//       // *important!!! preservation of structure
+//       ...state,
+//       counter: state.counter + 1,
+//     });
+//   };
+//   console.log(state);
+//   return (
+//     <button onClick={handlerClick}> На меня нажали {state.counter}</button>
+//   );
+// }
+
+// function App() {
+//   const [counters, setCounters] = useState([0, 0, 0]);
+
+//   // const count = (n) => {
+//   //   setCounters(
+//   //     counters.map((v, i) => {
+//   //       if (n === i) {
+//   //         return v + 1;
+//   //       }
+
+//   //       return v;
+//   //     })
+//   //   );
+//   // };
+//   //
+//   const count = (n) => setCounters(counters.map((x, i) => x + (i === n)));
+
+//   //change State setCounter next setCounter(1) next setCounter(2)  === Reactivity
+//   return (
+//     <ul>
+//       {[0, 1, 2].map((v) => (
+//         <li key={v}>
+//           <button onClick={() => count(v)}>
+//             На меня нажали {counters[v]} раз
+//           </button>
+
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export default App;
+
+// function App() {
+//   const [cities, setCities] = useState([
+//     { name: "Moscow", description: "Столица России" },
+//     { name: "Zelenograd", description: "Научный спутник москвы" },
+//   ]);
+
+//   // the index of the city we want to display
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   const handlerChangeCity = (n, description) => {
+//     setCities(
+//       cities.map((city, index) => {
+//         if (index === n) {
+//           return {
+//             ...city,
+//             description,
+//           };
+//         }
+//         return city;
+//       })
+//     );
+//   };
+
+//   const handlerSelectCity = (n) => setCurrentIndex(n)
+
+//   return (
+//     <div>
+//       <City
+//         cities={cities}
+//         onChangeCity={handlerChangeCity}
+//         currentIndex={currentIndex}
+//       />
+//       <CityesList cities={cities} onSelectCity ={handlerSelectCity}/>
+//     </div>
+//   );
+// }
 
 // function App(props) {
 //   const [value, setValue] = useState("Яблоко");
@@ -110,4 +176,5 @@ function App() {
 //     );
 //   }
 // }
+
 export default App;
